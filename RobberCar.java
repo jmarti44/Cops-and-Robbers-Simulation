8505d2f3acbdab8c0bbc532fc65c2a1578705d37
Jose Martinez <79774071+jmarti44@users.noreply.github.com>
2021-03-05 17:10:27 -0600

    Update RobberCar.java

--- RobberCar.java
+++ RobberCar.java
@@ -1,39 +1,72 @@
 import java.awt.Graphics;
 import java.util.Random;
-public class RobberCar extends Car 
+public class CopCar extends Car 
 {
-    private int xRatio;
-    private int yRatio;
-    RobberCar()
-    {
-        super ("RobberCar", 5000, new Engine("V8",20,200),"red-car.jpg");
+    private static int xRatio;
+    private static int yRatio;
+    private int newX =1;
+    private int newY =1;
+    Graphics g;
+
+    Sprite newCar;
+    CopCar()
+    {                    
+        super ("CopCar",5000, new Engine("V12",30,100),"cop-car.jpg");
+
         Random ranX = new Random();
         Random ranY = new Random();
 
         xRatio = ranX.nextInt(5+5) -5;  //(max-min) + min
         yRatio = ranY.nextInt(5+5) - 5;
+
+
         this.fillUp();
     }
+    CopCar(int x, int y)
+    {
+        super ("CopCar",5000, new Engine("V12",30,100),"cop-car.jpg");
+  
+        this.fillUp();
 
+    }
+  
     public void updateImage(Graphics g)
     {
     
-        //grabbing x and y from the grandparent class and updating location
-
-        /**
-        setx((int)getx() + xRatio);
-        sety((int)gety() + yRatio);
-        */
-        
-   
         super.updateImage(g);
 
     }
+ 
     public void updateState(int width, int height)
     {
-        drive(40, xRatio, yRatio);
+       
+        if (this.gety() <= 0 || this.gety() > height)
+        {
+
+            newX = (-1) * newX;
+            newY = (-1) * newY;
+        }
+        else if (this.getx() <=0 || this.getx() > width)
+        {
+            newX = (-1) * newX;
+            newY = (-1) * newY;
+        }
+       
+
+        super.drive(2,newX * xRatio, newY * yRatio);
         setx((int)getx());
         sety((int)gety());
+    
     }
 
+
+
 }
+    
+       
+
+       
+    
+    
+
+
